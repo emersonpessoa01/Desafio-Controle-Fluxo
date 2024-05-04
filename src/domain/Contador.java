cb1134d01fc7adf61c47271f3bcae26bf1f898a8
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2024-05-04 17:25:52 -0300

    fix : adjusted the comment in the Contador :sparkles:

--- src/domain/Contador.java
+++ src/domain/Contador.java
@@ -5,27 +5,27 @@ import exception.ParametrosInvalidosException;
 import java.util.Scanner;
 
 public class Contador {
+    /**
+     * sistema deverá receber dois parâmetros via terminal que representarão dois
+     * números inteiros,
+     * com estes dois números você deverá obter a quantidade de interações (for) e
+     * realizar a impressão no console
+     * (System.out.print) dos números incrementados, exemplo: Se você passar os
+     * números 12 e 30,
+     * logo teremos uma interação (for) com 18 ocorrências para imprimir os números,
+     * exemplo: "Imprimindo o número 1", "Imprimindo o número 2" e assim por diante.
+     * Se o primeiro parâmetro for MAIOR que o segundo parâmetro, você deverá lançar
+     * a exceção customizada
+     * chamada de ParametrosInvalidosException com a segunda mensagem: "O segundo
+     * parâmetro deve ser maior que o primeiro."
+     * Scanner input = new Scanner(System.in);
+     */
     public static void main(String[] args) {
-        /**
-         * sistema deverá receber dois parâmetros via terminal que representarão dois
-         * números inteiros,
-         * com estes dois números você deverá obter a quantidade de interações (for) e
-         * realizar a impressão no console
-         * (System.out.print) dos números incrementados, exemplo: Se você passar os
-         * números 12 e 30,
-         * logo teremos uma interação (for) com 18 ocorrências para imprimir os números,
-         * exemplo: "Imprimindo o número 1", "Imprimindo o número 2" e assim por diante.
-         * Se o primeiro parâmetro for MAIOR que o segundo parâmetro, você deverá lançar
-         * a exceção customizada
-         * chamada de ParametrosInvalidosException com a segunda mensagem: "O segundo
-         * parâmetro deve ser maior que o primeiro."
-         * Scanner input = new Scanner(System.in);
-         */
 
         System.out.println("-----------------Desafio Controle de fluxo-----------------");
         System.out.println("Digite dois valores para saber o número de interações entre eles.");
         System.out.println("1º valor: ");
-        Scanner scanner =  new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int paramentro1 = lerInteiro(scanner);
 
         System.out.println("2º valor: ");
